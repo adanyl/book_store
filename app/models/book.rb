632fe8f58894a6ac108b50fd3dc5a314d79016c6
Oleksii Danylenko <alexey.danylenko@gmail.com>
2023-11-16 00:02:19 +0200

    [Feature] add annotate gem

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id             :bigint           not null, primary key
+#  author         :string           not null
+#  genre          :string
+#  price          :float            not null
+#  published_year :integer
+#  title          :string           not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
 class Book < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :order_items
