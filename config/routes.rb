4ff01b9e2f9eb6f87a06aa2a8032e6c96611fc7d
Oleksii Danylenko <alexey.danylenko@gmail.com>
2023-11-15 21:52:04 +0200

    [Feature] showing orders history, confirm draft order

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :books, only: [:index, :show]
   resources :reviews, except: [:index]
   resources :order_items, only: [:destroy]
+  resources :orders, only: [:index, :show, :update]
 
   post 'checkout/create_order_item', to: 'checkout#create_order_item', as: 'create_order_item'
   get 'checkout/show_draft_order', to: 'checkout#show_draft_order', as: 'show_draft_order'
