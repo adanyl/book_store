b9552e8a9353db5ec48157961927b9a152f47bc2
Oleksii Danylenko <alexey.danylenko@gmail.com>
2023-11-15 15:58:14 +0200

    [Feature] add creating reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,6 +3,22 @@ class ReviewsController < ApplicationController
   
     def show
       @review = Review.find(params[:id])
+      @book = @review.book
+    end
+  
+    def new
+        @book = Book.find(params[:book_id])
+    end
+  
+    def create
+      review = Review.new(review_params)
+      review.user = current_user
+  
+      if review.save
+        redirect_to review_path(review), notice: 'Review was successfully created.'
+      else
+        redirect_to new_review_path(book_id: params[:book_id]), alert: 'Review can not be created'
+      end
     end
   
     def destroy
@@ -11,5 +27,11 @@ class ReviewsController < ApplicationController
       review.destroy
   
       redirect_to book_path(book), notice: 'Review was successfully deleted.'
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:rating, :comment, :book_id)
     end
   end
