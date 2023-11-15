66041095cc6c51d4693fdb6e0fdddd489401d14f
Oleksii Danylenko <alexey.danylenko@gmail.com>
2023-11-15 12:13:26 +0200

    [Feature] add book show page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,5 +2,11 @@ class BooksController < ApplicationController
 	def index
 		@books = Book.all
 	end
+
+	def show
+		@book = Book.find(params[:id])
+		@published_year = @book.published_year || 'N/A'
+    @genre = @book.genre || 'N/A'
+	end
 end
   
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   
-  get '/books' => 'books#index', as: 'books'
+  resources :books, only: [:index, :show]
 end
