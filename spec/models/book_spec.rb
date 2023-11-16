76f810cb03a971beb2e25a05bc4dfa7e903d1894
Oleksii Danylenko <alexey.danylenko@gmail.com>
2023-11-16 10:52:12 +0200

    [Fix] annotate book model test

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id             :bigint           not null, primary key
+#  author         :string           not null
+#  genre          :string
+#  price          :float            not null
+#  published_year :integer
+#  title          :string           not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
